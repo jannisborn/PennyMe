b6bdeb3ee6715b402dc82067726c34cdb9036b63
NinaWie <wnina@student.ethz.ch>
2023-04-10 23:01:41 +0200

    update version number

--- PennyMe/AboutViewController.swift
+++ PennyMe/AboutViewController.swift
@@ -22,7 +22,7 @@ class AboutViewController: UIViewController {
         self.label.contentMode = .scaleToFill
         self.label.numberOfLines = 30
 
-        self.label.text = "PennyMe makes collecting pennys easier than ever before - anywhere you travel. \n\nYou can view locations of nearby penny machines and explore your favorite destinations. Change the status of the pins and turn PennyMe into your digital penny collection. PennyMe also helps you to navigate to the next machine and provides pictures and more information about each machine. To ease your life, PennyMe can also send you push notifications if you are nearby an unvisted penny machine.  Please help growing our database by sending pictures or information about machine and feel free to send us any feedback. \n\nThis is PennyMe V1.2.\n©Jannis Born & Nina Wiedemann (2022)"
+        self.label.text = "PennyMe makes collecting pennys easier than ever before - anywhere you travel. \n\nYou can view locations of nearby penny machines and explore your favorite destinations. Change the status of the pins and turn PennyMe into your digital penny collection. PennyMe also helps you to navigate to the next machine and provides pictures and more information about each machine. To ease your life, PennyMe can also send you push notifications if you are nearby an unvisted penny machine.  Please help growing our database by sending pictures or information about machine and feel free to send us any feedback. \n\nThis is PennyMe V1.3.\n©Jannis Born & Nina Wiedemann (2023)"
     }
 }
 
