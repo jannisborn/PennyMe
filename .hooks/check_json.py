a36a75f1a21384fa51924f835ff46faa4393eea4
jannisborn <jannis.born@gmx.de>
2023-09-03 00:38:02 +0200

    ci: add pre-commit hook

---
+++ .hooks/check_json.py
@@ -0,0 +1,17 @@
+import json
+import os
+import sys
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+try:
+    with open(os.path.join(root_dir, "data", "server_locations.json"), "r") as f:
+        data = json.load(f)
+except Exception as e:
+    sys.exit(1)
+
+try:
+    with open(os.path.join(root_dir, "data", "all_locations.json"), "r") as f:
+        data = json.load(f)
+except Exception as e:
+    sys.exit(1)
+sys.exit(0)
