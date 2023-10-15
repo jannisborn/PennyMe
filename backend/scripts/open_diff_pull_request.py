951ad81e315b70fc241aed675f187c02c6181169
NinaWie <wnina@student.ethz.ch>
2023-10-16 00:20:34 +0200

    push also problems json file

--- backend/scripts/open_diff_pull_request.py
+++ backend/scripts/open_diff_pull_request.py
@@ -14,23 +14,40 @@ if __name__ == "__main__":
         type=str,
         default="/root/PennyMe/new_data/server_locations.json"
     )
+    parser.add_argument(
+        "-p",
+        "--problems_file",
+        type=str,
+        default="/root/PennyMe/new_data/problems.json"
+    )
     args = parser.parse_args()
 
-    # load new server location
+    # load new server location and problem file
     with open(args.file, "r") as infile:
         server_locations = json.load(infile)
+    # load new problems json
+    with open(args.problems_file, "r") as infile:
+        problems_json = json.load(infile)
 
     # get latest_commit_sha
     _, latest_commit_sha = load_latest_server_locations(
         branch_name=DATA_BRANCH
     )
 
-    commit_message = "Updates from website"
+    commit_message = "Updates from website "
 
     commit_json_file(
         server_locations,
         branch_name=DATA_BRANCH,
-        commit_message=commit_message,
+        commit_message=commit_message + "(server_locations)",
+        latest_commit_sha=latest_commit_sha,
+        headers=HEADER_LOCATION_DIFF
+    )
+
+    commit_json_file(
+        problems_json,
+        branch_name=DATA_BRANCH,
+        commit_message=commit_message + "(problems json)",
         latest_commit_sha=latest_commit_sha,
         headers=HEADER_LOCATION_DIFF
     )
