97453171fba38b70b49f9b8ee0e35203dbef857d
Nina Wiedemann <wnina@student.ethz.ch>
2023-10-20 12:18:19 +0000

    Machine updates (#184)
    * user comments
    
    * user comments
    
    * Updates from website (server_locations)
    
    * readd changes after they were destroyed by cron job
    
    * Updates from website (server_locations)
    
    * hot fix for problem that old server_location outputs are pushed to git
    
    * clean up paths
    
    * Update server_locations.json
    
    ---------
    
    Co-authored-by: jannisborn <jannis.born@gmx.de>

--- backend/data_update.sh
+++ backend/data_update.sh
@@ -5,15 +5,18 @@
 conda activate myenv
 
 # Define paths to the old and new JSON files
-CUR_JSON_FILE="/root/PennyMe/new_data/old_server_locations.json"
-OUT_JSON_FILE="/root/PennyMe/new_data/server_locations.json"
+OLD_JSON_FILE="/root/PennyMe/new_data/old_server_locations.json"
+NEW_JSON_FILE="/root/PennyMe/new_data/server_locations.json"
 OLD_PROBLEMS_JSON_FILE="/root/PennyMe/new_data/old_problems.json"
 NEW_PROBLEMS_JSON_FILE="/root/PennyMe/new_data/problems.json"
+DEBUG_PATH="/root/PennyMe/debug_new_data"
 
-python /root/PennyMe/backend/scripts/location_differ.py --load_from_github -o /root/PennyMe/new_data -d /root/PennyMe//data/all_locations.json -s ${CUR_JSON_FILE} -a ${GCLOUD_KEY}
+python /root/PennyMe/backend/scripts/location_differ.py --load_from_github -o /root/PennyMe/new_data -d /root/PennyMe/data/all_locations.json -s ${OLD_JSON_FILE} -a ${GCLOUD_KEY}
 
 
 # Check if there is a difference in at least one of the two files, if yes, open PR
-python /root/PennyMe/backend/scripts/open_diff_pull_request.py -f "$OUT_JSON_FILE" -p "$NEW_PROBLEMS_JSON_FILE"
+python /root/PennyMe/backend/scripts/open_diff_pull_request.py -f "$NEW_JSON_FILE" -p "$NEW_PROBLEMS_JSON_FILE"
 
-cp $NEW_PROBLEMS_JSON_FILE $OLD_PROBLEMS_JSON_FILE
+# Move the new files to make sure that they are not mistakenly used if the location_differ fails next time
+mv $NEW_PROBLEMS_JSON_FILE $DEBUG_PATH
+mv $NEW_JSON_FILE $DEBUG_PATH
