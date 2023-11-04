eb2024aadbaaecd95809c06dc1d255676192e649
jannisborn <jannis.born@gmx.de>
2023-11-05 00:01:01 +0100

    wip: updated utils for machine extraction

--- backend/pennyme/webconfig.py
+++ backend/pennyme/webconfig.py
@@ -8,3 +8,17 @@ def get_website(url: str):
     encoded_str = unicode_str.encode("ascii", "ignore")
     website = BeautifulSoup(encoded_str, "html.parser")
     return website
+
+
+def get_elongated_coin_title(url: str) -> str:
+    """
+    Get the title of a url pointing to a forum entry on elongated-coin.de.
+    """
+
+    mhtml = requests.get(url).content
+    unicode_str = mhtml.decode("utf8")
+    encoded_str = unicode_str.encode("ascii", "ignore")
+    website = BeautifulSoup(encoded_str, "html.parser")
+    raw_title = str(website.find("title"))
+    title = raw_title.split("Thema anzeigen - ")[-1].split("</title>")[0]
+    return title
