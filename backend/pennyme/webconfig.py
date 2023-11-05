0f86180595f91e9aa63899e1287663826ebf3494
jannisborn <jannis.born@gmx.de>
2023-11-06 00:12:19 +0100

    wip: utils for comment extraction from website

--- backend/pennyme/webconfig.py
+++ backend/pennyme/webconfig.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import List
 
 
 def get_website(url: str):
@@ -22,3 +23,29 @@ def get_elongated_coin_title(url: str) -> str:
     raw_title = str(website.find("title"))
     title = raw_title.split("Thema anzeigen - ")[-1].split("</title>")[0]
     return title
+
+
+def get_elongated_coin_comments(url: str) -> List[str]:
+    """
+    Extract comments/posts from an elongated-coin.de site.
+    """
+    mhtml = requests.get(url).content
+    unicode_str = mhtml.decode("utf8")
+    soup = BeautifulSoup(unicode_str, "html.parser")
+
+    # Find the last post using its class, assuming the class "post bg3" is for the last post.
+    posts = soup.find_all("div", class_="post")
+
+    comments = []
+    if len(posts) >= 2:
+        for post in posts[2:-1]:
+            post_content = post.find("div", class_="content")
+            if post_content:
+                c = (
+                    post_content.get_text()
+                    .replace("\n", " ")
+                    .replace("\t", " ")
+                    .strip()
+                )
+                comments.append(c)
+    return comments
