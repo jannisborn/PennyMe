22fc2ccf9fa7c6e514fd9185a2df206abfc6f3a7
NinaWie <wnina@student.ethz.ch>
2024-04-01 10:15:33 +0200

    test: expand hook to test for duplicates

--- .hooks/check_json.py
+++ .hooks/check_json.py
@@ -6,6 +6,12 @@ root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 try:
     with open(os.path.join(root_dir, "data", "server_locations.json"), "r") as f:
         data = json.load(f)
+
+    ids = [e["properties"]["id"] for e in data["features"]]
+    if not len(ids) == len(set(ids)):
+        raise ValueError(
+            f"Duplicate entries in server_locations {len(ids)} and {len(set(ids))}"
+        )
 except Exception as e:
     print("FAILURE!", e)
     sys.exit(1)
