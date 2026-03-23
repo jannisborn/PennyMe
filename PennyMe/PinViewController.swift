9b214d08153704bb3db97d987ba66c2cd83ed4c4
NinaWie <wnina@student.ethz.ch>
2026-03-24 00:16:04 +0100

    feat: use new duckdns link for images and json and make loading safer

--- PennyMe/PinViewController.swift
+++ PennyMe/PinViewController.swift
@@ -12,7 +12,7 @@ import SwiftUI
 
 
 let flaskURL = "http://37.120.179.15:6006/"
-let imageURL = "http://37.120.179.15:8000/"
+let imageURL = "https://pennyme.duckdns.org/"
 
 @available(iOS 13.0, *)
 class PinViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
